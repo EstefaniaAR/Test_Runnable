bbc42c60f6f076b087a9990b56c62292fb271226
Estefani <estefania@outlook.com>
2019-02-26 14:16:24 -0600

    Initial commit

---
+++ app/src/main/java/com/example/estefaniaar/test_runnable/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.estefaniaar.test_runnable
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var number =0
+    var handler = Handler()
+    var runnable= Runnable{}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun start (view:View)
+    {
+        number =0
+        runnable = object:Runnable {
+            override fun run()
+            {
+                textView.text = "Time: $number"
+                number++
+                textView.text = "Time: $number"
+                handler.postDelayed(this,1000)
+            }
+        }
+        handler.post (runnable )
+    }
+    fun reset (view:View)
+    {
+        handler.removeCallbacks(runnable)
+        number=0
+        textView.text = "Time: $number"
+
+    }
+}
